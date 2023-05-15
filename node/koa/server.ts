c0e284b482ab9be8e630672342a0132bdab44acb
xlsama <im.xlsama@gmail.com>
2023-05-15 11:25:51 +0800

    chore: init

---
+++ node/koa/server.ts
@@ -0,0 +1,25 @@
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async ctx => {
+  ctx.body = '1';
+  for (let i = 2; i < 10000; i++) {
+    ctx.body += i;
+  }
+});
+
+app.listen(3000);
