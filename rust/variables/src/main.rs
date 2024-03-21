6afc2f6b44406dae4cdd3ef9729bbb3ff6626d71
xlsama <im.xlsama@gmail.com>
2024-03-21 08:51:45 +0800

    chore: update

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -2,5 +2,10 @@ fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
-    println!("The value of x is: {}", x);
+    println!("the value of x is: {}", x);
+
+    let a = [1, 2, 3, 4, 5];
+    for element in a.iter() {
+        println!("the value is {}", element);
+    }
 }
